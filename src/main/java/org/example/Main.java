8ac615037c015c959e7c2e2149b15843fc2b077f
Jakub Kędzia <jkedzia99@gmail.com>
2026-09-08 01:22:01 +0200

    add some fixes and prepare the main method to run

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,12 @@ package org.example;
 
 public class Main {
     static void main() {
+        Ship ship = new Ship("Niszczyciel", 150, 5000);
+        String a = ship.toString();
+        System.out.println(a);
+
+        ship.shipUpdateFuel(0, 100);
+        String b = ship.toString();
+        System.out.println(b);
     }
     }
--- src/main/java/org/example/Ship.java
+++ src/main/java/org/example/Ship.java
@@ -1,30 +1,32 @@
 package org.example;
 
 public class Ship extends ShipTemplate {
-    private int startFuel = 100;
     private int startCurrentCapacity = 0;
 
-    public Ship(String name, int maxLoadCapacity) {
+    public Ship(String name, int maxFuel, int maxLoadCapacity) {
         super.setName(name);
-        super.setMaxFuel(startFuel);
+        super.setMaxFuel(maxFuel);
         super.setMaxLoadCapacity(maxLoadCapacity);
-        super.setCurrentTankStatus(startFuel);
+        super.setCurrentTankStatus(maxFuel);
         super.setCurrentLoadStatus(startCurrentCapacity);
     }
 
-    public void shipUpdate(int tankFuel, int fuelConsumption, int loadMass, int unloadMass) {
+    public void shipUpdateFuel(int tankFuel, int fuelConsumption) {
         this.setCurrentTankStatus(getCurrentTankStatus() + tankFuel);
         this.setCurrentTankStatus(getCurrentTankStatus() - fuelConsumption);
-        this.setCurrentLoadStatus(getCurrentLoadStatus() + loadMass);
-        this.setCurrentLoadStatus(getCurrentLoadStatus() - unloadMass);
+    }
+
+    public void shipUpdateCargo(int loadCargo, int unloadCargo) {
+        this.setCurrentLoadStatus(getCurrentLoadStatus() + loadCargo);
+        this.setCurrentLoadStatus(getCurrentLoadStatus() - unloadCargo);
     }
 
     @Override
     public String toString() {
         return "Ship{" +
                 "name=" + super.getName() +
-                ", fuel=" + super.getCurrentTankStatus() + " / " + super.getMaxFuel() +
-                ", cargo=" + super.getCurrentLoadStatus() + " / " + super.getMaxLoadCapacity() +
+                ", fuel=" + super.getCurrentTankStatus() + "/" + super.getMaxFuel() +
+                ", cargo=" + super.getCurrentLoadStatus() + "/" + super.getMaxLoadCapacity() +
                 '}';
     }
 }
