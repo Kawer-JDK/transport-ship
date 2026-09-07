b6cb23c29add0a5efa0ee34847f101015780885b
Jakub Kędzia <jkedzia99@gmail.com>
2026-09-08 01:25:03 +0200

    Added some examples for checking the correct operation of code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,13 @@ public class Main {
         ship.shipUpdateFuel(0, 100);
         String b = ship.toString();
         System.out.println(b);
+
+        ship.shipUpdateCargo(4500, 0);
+        String c = ship.toString();
+        System.out.println(c);
+
+        ship.shipUpdateCargo(1000, 0);
+        String d = ship.toString();
+        System.out.println(d);
     }
     }
