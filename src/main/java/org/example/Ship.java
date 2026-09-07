7bd0c7865510756bcbf4b42382f9410c16b42226
Jakub Kędzia <jkedzia99@gmail.com>
2026-09-08 00:27:50 +0200

    Added class Ship with private fields

---
+++ src/main/java/org/example/Ship.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Ship {
+    private String name;
+    private int maxFuel;
+    private int currentTankStatus;
+    private int maxLoadCapacity;
+    private int currentLoadStatus;
+}
