a00da820890f9a703e76686165dbf9b3b95221e2
Jakub Kędzia <jkedzia99@gmail.com>
2026-09-08 00:48:31 +0200

    added a constructor to ship class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,5 @@ package org.example;
 
 public class Main {
     static void main() {
-
-        }
+    }
     }
--- src/main/java/org/example/Ship.java
+++ src/main/java/org/example/Ship.java
@@ -4,9 +4,9 @@ public class Ship extends ShipTemplate {
     private int startFuel = 100;
     private int startCurrentCapacity = 0;
 
-    public Ship(String name, int maxFuel, int maxLoadCapacity) {
+    public Ship(String name, int maxLoadCapacity) {
         super.setName(name);
-        super.setMaxFuel(maxFuel);
+        super.setMaxFuel(startFuel);
         super.setMaxLoadCapacity(maxLoadCapacity);
         super.setCurrentTankStatus(startFuel);
         super.setCurrentLoadStatus(startCurrentCapacity);
